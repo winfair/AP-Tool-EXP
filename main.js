2c2dfb9afd61b1715c490a1fa66b6ed1eedd1557
winfair <winfair92@yahoo.com>
2025-11-06 20:47:45 -0800

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,12 @@
 import { GeoMotionService } from './geoMotion.js';
 import { OrientationFrame } from './orientationFrame.js';
 import { SensorUI } from './ui.js';
+import { computeAzimuthFromDeviceAxes } from './azimuthProjection.js';
 
 const ui = new SensorUI();
 const svc = new GeoMotionService();
+const frame = new OrientationFrame({ declinationDeg: 0 }); // change if you want real declination
 
-// set your actual declination here if you want real map north
-const frame = new OrientationFrame({ declinationDeg: 0 });
-
-// wire sensor callbacks
 svc.onGeo = (data) => {
   ui.setGeoStatus('watching position');
   ui.showGeo(data);
@@ -29,7 +27,16 @@ svc.onMotion = (data) => {
   frame.updateMotion(data);
 
   const axes = frame.getDeviceAxes();
-  if (axes) ui.showAxes(axes);
+  if (axes) {
+    ui.showAxes(axes);
+
+    const azInfo = computeAzimuthFromDeviceAxes(axes);
+    if (azInfo && azInfo.azimuthDeg != null) {
+      ui.drawAzimuth(azInfo.azimuthDeg);
+    } else {
+      ui.drawAzimuth(null);
+    }
+  }
 
   const heading = frame.getTrueHeadingDeg();
   if (heading != null) ui.showHeading(heading);
